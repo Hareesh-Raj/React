8913e9c8f1fa5c784c84f35d6326b2ae824f6c73
Hareesh-Raj <125631814+Hareesh-Raj@users.noreply.github.com>
2023-04-18 17:02:00 +0530

    Modified Styles

--- ExplorerAPI/src/components/banner.components.js
+++ ExplorerAPI/src/components/banner.components.js
@@ -16,7 +16,7 @@ function Banner() {
             <span className={style.bolder}>SOUTH</span>
           </h1>
           <Dropdown change={(e) => setExploreCity(e.target.value)} />
-          <Button value="EXPLORE" path={`/details/${exploreCity}`} />
+          <Button value="EXPLORE" path={`/details/${exploreCity.toLowerCase()}`} />
         </div>
         <div className={style.explore_banner}>
           <img src={banner} alt="Explorer Banner"></img>
